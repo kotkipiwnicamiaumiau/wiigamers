849b66a5fbb38b52d33e4518ef122adc79ec8c4d
Igor Dmochowski <35695375+SirLemoniada@users.noreply.github.com>
2020-06-06 19:22:21 +0200

    Update summarize_wybiorcze.py

--- summarize_wybiorcze.py
+++ summarize_wybiorcze.py
@@ -41,5 +41,3 @@ def summarize(text):
             i+=1
             summary.append(sentence)
     return summary
-
-print(summarize('''A planned memorial was underway for George Floyd in Raeford, N.C., on Saturday morning, as demonstrations in his memory and against police brutality began in cities nationwide. Hundreds of thousands of people were expected to take to the streets nationwide, including Washington, D.C., and other major cities, as well as in countless smaller cities and towns in between. Peter Newsham, the chief of police in Washington, expected the demonstration to be “one of the largest” the city has seen.'''))

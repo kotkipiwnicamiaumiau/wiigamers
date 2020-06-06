49b34509f439b67c5cbe14644a769a0ace7ce28a
mbknust <maks@knust.one>
2020-06-06 17:23:01 +0200

    fixed python formatting (autopep8)

--- login.py
+++ login.py
@@ -1,42 +1,59 @@
-import hashlib, os, binascii, sqlite3
+import hashlib
+import os
+import binascii
+import sqlite3
+
 
 def hash(password):
     salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
-    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), salt, 100000)
+    pwdhash = hashlib.pbkdf2_hmac(
+        'sha512', password.encode('utf-8'), salt, 100000)
     pwdhash = binascii.hexlify(pwdhash)
     return (salt + pwdhash).decode('ascii')
+
 
 def verify(stored_password, provided_password):
     salt = stored_password[:64]
     stored_password = stored_password[64:]
-    pwdhash = hashlib.pbkdf2_hmac('sha512', provided_password.encode('utf-8'),salt.encode('ascii'),100000)
+    pwdhash = hashlib.pbkdf2_hmac(
+        'sha512',
+        provided_password.encode('utf-8'),
+        salt.encode('ascii'),
+        100000)
     pwdhash = binascii.hexlify(pwdhash).decode('ascii')
     return pwdhash == stored_password
+
 
 def createdb():
     db = sqlite3.connect("kotki.db")
     cursor = db.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS users(name TEXT,password TEXT)''')
-    cursor.execute('''CREATE TABLE IF NOT EXISTS videos(title TEXT,id TEXT, summary TEXT, transcript TEXT)''')
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS users(name TEXT,password TEXT)''')
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS videos(title TEXT,id TEXT, summary TEXT, transcript TEXT)''')
     db.commit()
     db.close()
+
 
 def insertuser(username, hash):
     db = sqlite3.connect("kotki.db")
     cursor = db.cursor()
     createdb()
-    cursor.execute('''INSERT INTO users(name,password) VALUES(?,?)''', (username,hash))
+    cursor.execute(
+        '''INSERT INTO users(name,password) VALUES(?,?)''', (username, hash))
     db.commit()
     db.close()
 
+
 def signup_f(user, passwd):
     try:
-        passwdhash=hash(passwd)
-        insertuser(user,passwdhash)
+        passwdhash = hash(passwd)
+        insertuser(user, passwdhash)
         createuserdb(user)
-    except:
+    except BaseException:
         return False
     return True
+
 
 def login_f(user, passwd):
     if passwd == "":
@@ -45,9 +62,9 @@ def login_f(user, passwd):
     cursor = db.cursor()
     cursor.execute('''SELECT password FROM users WHERE name=?''', (user,))
     users = cursor.fetchone()
-    if users == None:
+    if users is None:
         return False
-    user1=users[0]
+    user1 = users[0]
     db.commit()
     db.close()
     return verify(user1, passwd)
--- utils.py
+++ utils.py
@@ -1,10 +1,14 @@
 from google.cloud import speech_v1p1beta1
 from google.cloud.speech_v1p1beta1 import enums
 from google.cloud import storage
-import random, string
+import random
+import string
 from moviepy.editor import *
 
-#transkrybowanie pliku audio. Przyjmuje gcloud-storage uri, zwraca (plaintext, zdania z timestampami)
+# transkrybowanie pliku audio. Przyjmuje gcloud-storage uri, zwraca
+# (plaintext, zdania z timestampami)
+
+
 def transcribe(storage_uri):
     """
     Performs synchronous speech recognition on an audio file
@@ -47,22 +51,28 @@ def transcribe(storage_uri):
     print(u"Waiting for operation to complete...")
     response = operation.result()
 
-
     for result in response.results:
         # First alternative is the most probable result
         alternative = result.alternatives[0]
 
-        plaintext += alternative.transcript + ' ';
-        sentences.append((alternative.transcript, (alternative.words[0].start_time.seconds, alternative.words[0].start_time.nanos)))
+        plaintext += alternative.transcript + ' '
+        sentences.append(
+            (alternative.transcript,
+             (alternative.words[0].start_time.seconds,
+              alternative.words[0].start_time.nanos)))
     return (plaintext, sentences)
 
 
-#wrzucanie plików do cloud storage.
+# wrzucanie plików do cloud storage.
 def upload_blob(source_file_name):
     """Uploads a file to the bucket."""
     bucket_name = "alamakota1"
     # source_file_name = "local/path/to/file"
-    destination_blob_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+    destination_blob_name = ''.join(
+        random.choices(
+            string.ascii_uppercase +
+            string.digits,
+            k=5))
 
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
@@ -74,18 +84,21 @@ def upload_blob(source_file_name):
     return link
 
 
-#extracts .mp3 file from video and returns it's relative path
+# extracts .mp3 file from video and returns it's relative path
 def extract_audio(source_file_name):
     video = VideoFileClip(source_file_name)
-    audio = video.audio 
-    path = 'audio/'+''.join(random.choices(string.ascii_uppercase + string.digits, k=5))+'.mp3'
+    audio = video.audio
+    path = 'audio/' + \
+        ''.join(random.choices(string.ascii_uppercase + string.digits, k=5)) + '.mp3'
     audio.write_audiofile(path)
     return path
+
 
 def test(res):
     print(res[0])
     print("------------------------------------------")
     print(res[1])
 
-#example
+
+# example
 test(transcribe(upload_blob(extract_audio("test.mp4"))))

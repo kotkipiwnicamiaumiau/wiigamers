75954415b3b5304d6af2abe4e3731cad77c1bf8c
khknopp <kknopp@staszic.waw.pl>
2020-06-06 14:51:19 +0200

    Added app routes

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ app.secret_key = '9je0jaj09jk9dkakdwjnjq'
 
 @app.route('/')
 def main():
+    return redirect(url_for('upload'))
+
+@app.route('/upload')
+def upload():
     return render_template('upload.html')
+
+@app.route('/view')
+def view():
+    return render_template('view.html')
 
 if __name__=='__main__':
     app.run(debug=True)

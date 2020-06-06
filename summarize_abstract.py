c673bb986dd7697541844c687a80f8272ea57cf3
Adam Szokalski <szokalskiadam@gmail.com>
2020-06-06 22:11:51 +0200

    fix dlugiego ladowania

--- summarize_abstract.py
+++ summarize_abstract.py
@@ -7,4 +7,3 @@ def summarize_title(text):
     while(i=="i" or i[0]=="<" or i[0]=="!"):
         i=summarizer.run(sentence)
     return i
-print(summarize_title('''A planned memorial was underway for George Floyd in Raeford, N.C., on Saturday morning, as demonstrations in his memory and against police brutality began in cities nationwide. Hundreds of thousands of people were expected to take to the streets nationwide, including Washington, D.C., and other major cities, as well as in countless smaller cities and towns in between. Peter Newsham, the chief of police in Washington, expected the demonstration to be “one of the largest” the city has seen.'''))

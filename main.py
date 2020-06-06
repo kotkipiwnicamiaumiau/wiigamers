dfbbc05d5f75cdcba13bf173c052e436dde0084a
Adam Szokalski <szokalskiadam@gmail.com>
2020-06-06 20:58:46 +0200

    file upload

--- main.py
+++ main.py
@@ -4,28 +4,57 @@ import random
 import hashlib
 import sys
 import json
+import string
 from flask import Flask, flash, render_template, redirect, request, url_for, jsonify, session, Response
 from login import signup_f, login_f
+from werkzeug.utils import secure_filename
+from utils import handle_video
+
+UPLOAD_FOLDER = 'static/vid'
+ALLOWED_EXTENSIONS = {'mp4'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 app = Flask(__name__)
 app.secret_key = '9je0jaj09jk9dkakdwjnjq'
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def main():
     return redirect(url_for('upload'))
 
 
-@app.route('/upload')
+@app.route('/upload', methods=["GET", 'POST'])
 def upload():
-    if 'username' in session:
-        return render_template('upload.html', username=session.get('username'))
-    else:
-        return redirect(url_for('login'))
+    if request.method == "GET":
+        if 'username' in session:
+            return render_template('upload.html', username=session.get('username'))
+        else:
+            return redirect(url_for('login'))
+    elif request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            print("bbs")
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            rand_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+            filename = secure_filename(rand_id + '.mp4')
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            handle_video(rand_id)
+            return redirect(url_for('viewall'))
 
 
 @app.route('/viewall')
-def view():
+def viewall():
     return render_template('viewall.html')
 
 
--- utils.py
+++ utils.py
@@ -3,7 +3,10 @@ from google.cloud.speech_v1p1beta1 import enums
 from google.cloud import storage
 import random
 import string
+import json
 from moviepy.editor import *
+from summarize_abstract import summarize_title
+from summarize_wybiorcze import summarize
 
 # transkrybowanie pliku audio. Przyjmuje gcloud-storage uri, zwraca
 # (plaintext, zdania z timestampami)
@@ -78,19 +81,23 @@ def upload_blob(source_file_name):
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
 
+    print("uploading")
     blob.upload_from_filename(source_file_name)
     link = f"gs://alamakota1/{destination_blob_name}"
+    print("uploaded")
 
     return link
 
 
 # extracts .mp3 file from video and returns it's relative path
 def extract_audio(source_file_name):
+    print("extracting")
     video = VideoFileClip(source_file_name)
     audio = video.audio
-    path = 'audio/' + \
+    path = 'static/audio/' + \
         ''.join(random.choices(string.ascii_uppercase + string.digits, k=5)) + '.mp3'
     audio.write_audiofile(path)
+    print("extracted")
     return path
 
 
@@ -99,6 +106,27 @@ def test(res):
     print("------------------------------------------")
     print(res[1])
 
+def handle_video(video_id):
+    filepath = 'static/vid/'+video_id+'.mp4'
+    #to trwa długo, więc jeśli chcecie testować to sobie zapiszcie wartosci
+    (plaintext, sentences_timestamps) = transcribe(upload_blob(extract_audio(filepath)))
+
+    #magia maksa i kajtka
+    title=summarize_title(plaintext)
+    summary_sentences = summarize(plaintext) #zwraca tablice zdań
+    summary=[("zdanie1", 5), ("zdanie2", 70)] #tutaj maks użyj mojej tablicy sentences_timestamps i tablicy summary_sentences
+    transcript=["zdania dla pierwszej minuty", "zdania dla drugiej minuty", "zdania dla trzeciej minuty"] #tutaj posortuj zdania z transcript co minute
+
+    summary_json = json.dumps(summary)
+    transcript_json = json.dumps(transcript)
+
+    db = sqlite3.connect("kotki.db")
+    cursor = db.cursor()
+    cursor.execute(
+        '''INSERT INTO videos(title,id, summary,transcript) VALUES(?,?,?,?)''', (time, video_id, summary_json, transcript_json))
+    db.commit()
+    db.close()
+    
 
 # example
-test(transcribe(upload_blob(extract_audio("test.mp4"))))
+#test(transcribe(upload_blob(extract_audio("vid/A2G1Z.mp4"))))
